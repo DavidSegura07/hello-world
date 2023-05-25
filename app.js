442f96dca4e73b814f16663959f1884378587d5b
‘David <dsegura07@yahoo.com>
2023-05-25 11:57:04 -0700

    fixing web

---
+++ app.js
@@ -0,0 +1,13 @@
+alert ("HELLO!");
+
+let UserResponse = prompt("Do you like cars?");
+
+if (UserResponse=="yes"){
+    console.log("Huzzah! Cars are awesome");}
+    else if (UserResponse=="yeah")
+    {console.log("Hecks yeah, Cars are awesome.");}
+else {console.log("what?!")}
+
+function myfavoriteColor({
+    prompt(WHat's my favorite color?)
+})
