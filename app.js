b48eb290ac0a875995d5e2665629151899477842
David Segura <134030596+DavidSegura07@users.noreply.github.com>
2023-05-25 22:12:19 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,39 @@
-alert ("HELLO!");
+alert("Car Enthusiast only!");
+function getname(){
+  let theirName = prompt("What is your name")
+  return theirName;
+}
 
-let UserResponse = prompt("Do you like cars?");
+function sayHi (name){
+  alert("Hello there, " + name + "!" );
+}
+sayHi(getname());
 
-if (UserResponse=="yes"){
-    console.log("Huzzah! Cars are awesome");}
-    else if (UserResponse=="yeah")
-    {console.log("Hecks yeah, Cars are awesome.");}
-else {console.log("what?!")}
+for(let i = 0; i< 10; i++){
+  let tireGuess = prompt("How many tires does a car have?");
+  if (parseInt(tireGuess) == 4){
+    alert("That's correct")
+    break;
+  }
+  else{
+    alert("Try again")
+  }
+}
 
-function myfavoriteColor({
-    prompt(WHat's my favorite color?)
-})
+// const userName = prompt("what is your name?");
+// if (userName == "Kassie"){
+//     document.write("Hi Teach!");
+//   } else if (userName == "David") {
+//     document.write("Hi " + userName + "!");
+//   }else if (userName == "Luke") {
+//     document.write("Hi " + userName + "!");
+//   } else if (userName == "Adnan") {
+//     document.write("Hi " + userName + "!");
+//   } else if (userName == "Dexter") {
+//     document.write("Hi " + userName + "!");
+//   } else if (userName == "Tonya") {
+//     document.write("Hi " + userName + "!");
+//   }
+//   else {
+//     document.write("Welcome!");
+//   }
